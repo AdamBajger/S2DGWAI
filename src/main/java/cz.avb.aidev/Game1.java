2f6060ef7ae7697a0cd2f4eaa06851682d6c74a6
Adam Bajger <xbajger@fi.muni.cz>
2018-10-01 16:44:39 +0200

    Added libraries, created main method, simple setup.

---
+++ src/main/java/cz.avb.aidev/Game1.java
@@ -0,0 +1,29 @@
+package cz.avb.aidev;
+
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game1 extends BasicGame {
+
+    public Game1() {
+        super("Second Game Try");
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
---
+++ src/main/java/cz.avb.aidev/Main.java
@@ -0,0 +1,19 @@
+package cz.avb.aidev;
+
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.SlickException;
+
+public class Main {
+    public static void main (String[] args) {
+        Game1 game = new Game1();
+
+        try {
+            AppGameContainer container = new AppGameContainer(game);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+}
