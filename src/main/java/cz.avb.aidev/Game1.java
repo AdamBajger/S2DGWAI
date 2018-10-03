2a489baf2598cb632b17f599107731d6fa0b9227
Adam Bajger <adambaj@seznam.cz>
2018-10-03 23:52:05 +0200

    added sprites for a cell and declared its animations

--- src/main/java/cz.avb.aidev/Game1.java
+++ src/main/java/cz.avb.aidev/Game1.java
@@ -1,23 +1,42 @@
 package cz.avb.aidev;
 
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class Game1 extends BasicGame {
     private TiledMap map;
 
 
+    private Animation cell;
+
+
     public Game1() {
         super("Second Game Try");
+        Image[] images =  new Image[8];
+        for (int i = 1; i <= 8; i++) {
+            try {
+                images[i] = new Image("src/main/resources/sprites/cellBasic/stand0" + i + ".png");
+            } catch (SlickException e) {
+                e.printStackTrace();
+            }
+        }
+
+        cell = new Animation(
+                images,
+                new int[] {300, 300, 300, 300, 300, 300, 300, 300},
+                true);
+
+
+
     }
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
         map = new TiledMap("src/main/resources/Game1map01.tmx");
+
+
+
     }
 
     @Override
