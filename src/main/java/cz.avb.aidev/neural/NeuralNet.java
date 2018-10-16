834335c29b84f0037efae7d4ef51bfcde5fc98c1
Adam Bajger <xbajger@fi.muni.cz>
2018-10-16 12:03:01 +0200

    Added method for deriving evolved matrices. Pinned a method for deriving the whole evolved net.

--- src/main/java/cz.avb.aidev/neural/NeuralNet.java
+++ src/main/java/cz.avb.aidev/neural/NeuralNet.java
@@ -20,6 +20,24 @@ public class NeuralNet {
     private DoubleMatrix[] hiddenLayers;
     private DoubleMatrix outputLayer;
 
+    public NeuralNet(
+            int inputLength,
+            int outputLength,
+            int hiddenLayersCount,
+            int hiddenLayerLength,
+            DoubleMatrix inputLayer,
+            DoubleMatrix[] hiddenLayers,
+            DoubleMatrix outputLayer
+    ) {
+        this.inputLength = inputLength;
+        this.outputLength = outputLength;
+        this.hiddenLayersCount = hiddenLayersCount;
+        this.hiddenLayerLength = hiddenLayerLength;
+        this.inputLayer = inputLayer;
+        this.hiddenLayers = hiddenLayers;
+        this.outputLayer = outputLayer;
+    }
+
     public NeuralNet(int inputLength, int outputLength, int hiddenLayersCount, int hiddenLayerLength) {
         this.inputLength = inputLength;
         this.outputLength = outputLength;
@@ -45,5 +63,32 @@ public class NeuralNet {
         }
         return result.mul(outputLayer);
 
+    }
+
+    /**
+     * Creates new matrix with slightly changed values from inputted matrix.
+     * Values are changed by a random values between {@code +evolutionStep}(inclusive) and {@code -evolutionStep}(exclusive)
+     *
+     * This method creates random matrix of dimensions matching inputted matrix, subtracts 0.5 from it,
+     * then multiplies it by 2, which results in matrix with random values ranging between -1 and +1.
+     * After that, the random matrix is multiplied by {@code evolutionStep} which results in matrix with random values
+     * described above.
+     *
+     * @param dm matrix to be evolved from
+     * @param maxEvolutionStep the bounds for the evolution steepness
+     * @return evolved matrix
+     */
+    private DoubleMatrix evolveMatrix(DoubleMatrix dm, double maxEvolutionStep) {
+        return dm.add(DoubleMatrix.rand(dm.rows, dm.columns).sub(0.5d).mul(2 * maxEvolutionStep));
+    }
+
+    public NeuralNet deriveDescendants(int numberOfDescendants, double maxEvolutionStep) {
+        //int inputLength
+        //int outputLength,
+        //int hiddenLayersCount,
+        //int hiddenLayerLength,
+        DoubleMatrix inputLayer = evolveMatrix(this.inputLayer, maxEvolutionStep);
+        DoubleMatrix[] hiddenLayers,
+        DoubleMatrix outputLayer
     }
 }
